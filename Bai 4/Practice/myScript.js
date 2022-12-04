56ff86943504f89be8867fadac494e4597b35ad9
AnhKhoa <khoa.mak246@gmail.com>
2022-12-04 14:04:09 +0700

    Add practice bai 4

---
+++ Bai 4/Practice/myScript.js
@@ -0,0 +1,19 @@
+// Practice 1
+function showmessage() {
+    window.alert("Xin chao");
+}
+
+// Practice 2
+function show_div_message() {
+    document.getElementById('messagediv').innerHTML = "Xin chao";
+}
+
+//Practice 3
+function show_document_write() {
+    document.writeln("Xin chao")
+}
+
+//Practice 4
+function show_consolelog_message() {
+    console.log("Xin chao")
+}
